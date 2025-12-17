df304c5339fcab4a9199ec763d3de6764dd13317
kokutoupan <kokutou2004@gmail.com>
2025-12-17 22:10:00 +0900

    buildの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-export default defineConfig({
-  // リポジトリ名を設定（これは必須）
-  base: '/webgpu-raytracer/',
-  build: {
-    // 出力先を 'docs' フォルダに変更
-    outDir: 'docs',
-  },
-  plugins: [
-    wasm(),
-    topLevelAwait()
-  ]
+export default defineConfig(({ mode }) => {
+  const isCloudflare = process.env.CF_PAGES === '1';
+
+  return {
+    base: isCloudflare ? '/' :'webgpu-raytracer/', // Cloudflare用（GitHub Pages用にはローカルビルド時に別途調整が必要かもですが一旦ルートで）
+    build: {
+      // Cloudflareなら 'dist' (デフォルト)、それ以外(ローカル)なら 'docs'
+      outDir: isCloudflare ? 'dist' : 'docs',
+    },
+    plugins: [
+      wasm(),
+      topLevelAwait()
+    ]
+  };
 });
